e13a873c8e0fd6de2d9b0cf8f9c02a9f8bada06e
Alexander Nnakwue <annakwue@terragonltd.com>
2020-10-30 08:40:08 +0100

    update code

--- server/app/db/pool.js
+++ server/app/db/pool.js
@@ -3,7 +3,7 @@ import { Pool } from 'pg'
 import dotenv from 'dotenv'
 
 dotenv.config()
-
+console.log(process.env.PG_URL, 'PG_URL')
 const databaseConfig = { connectionString: process.env.PG_URL }
 const pool = new Pool(databaseConfig)
 
